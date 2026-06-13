3d1f844010418f312cbe738b81335747b3acc4ac
X5YD <github@akbaridev.ir>
2026-06-14 00:59:43 +0330

    Optimized code and easier to read

--- dice.py
+++ dice.py
@@ -36,6 +36,8 @@ def dice_ascii():
     }
     return dice_art
 
+dice_art = dice_ascii()
+
 def dice_condition(bot_dice, user_dice):
         if user_dice < bot_dice:
             return "You Lost...!"
@@ -47,42 +49,35 @@ def dice_condition(bot_dice, user_dice):
 
 
 def dice_game():
-    dice_art = dice_ascii()
     score = 0
     bot_score = 0
-    while True:
-            score = 0
-            bot_score = 0
-            while True:
-                bot_dice = random.randint(1, 6)
-                user_dice = random.randint(1, 6)
-                time.sleep(1)
-                print(f"Bot Dice : {bot_dice}")
-                for bot_value in dice_art.get(bot_dice):
-                    print(bot_value)
-                time.sleep(1)
-                print(f"Your Dice : {user_dice}")
+    while score < 5 and bot_score < 5:
 
-                for user_value in dice_art.get(user_dice):
-                    print(user_value)
+        bot_dice = random.randint(1, 6)
+        user_dice = random.randint(1, 6)
+        time.sleep(1)
+        print(f"Bot Dice : {bot_dice}")
+        for bot_value in dice_art.get(bot_dice):
+            print(bot_value)
+        time.sleep(1)
+        print(f"Your Dice : {user_dice}")
 
-                result = dice_condition(bot_dice, user_dice)
-                if result == "You Won...!":
-                    score += 1
-                elif result == "You Lost...!":
-                    bot_score += 1
-                print(result)
-                if score == 5:
-                    print("You Won The Match")
-                    break
-                elif bot_score == 5:
-                    print("Bot Won The Match")
-                    break
-            time.sleep(1)
-            play_again = input("Do You Want To Play Again? [y/n] : ")
-            if play_again.lower().strip() == "y":
-                    continue
-            else:
-                break
+        for user_value in dice_art.get(user_dice):
+            print(user_value)
+
+        result = dice_condition(bot_dice, user_dice)
+        if result == "You Won...!":
+            score += 1
+        elif result == "You Lost...!":
+            bot_score += 1
+        print(result)
+    if score == 5:
+        print("You Won The Match")
+    elif bot_score == 5:
+        print("Bot Won The Match")
+    time.sleep(1)
+    play_again = input("Do You Want To Play Again? [y/n] : ")
+    if play_again.lower().strip() == "y":
+        dice_game()
 if __name__ == "__main__":
     dice_game()
